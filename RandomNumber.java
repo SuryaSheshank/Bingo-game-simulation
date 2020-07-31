08d98475dbedecb307b430eb1a372e1ee472c32b
SuryaSheshank <59456199+SuryaSheshank@users.noreply.github.com>
2020-07-31 14:10:44 +0530

    Add files via upload

---
+++ RandomNumber.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class RandomNumber {
+    static int randnum;
+
+    public static int getRandnum() {
+        return randnum;
+    }
+
+    public static void setRandnum(int randnum) {
+        RandomNumber.randnum = randnum;
+    }
+}
